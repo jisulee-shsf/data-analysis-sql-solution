508a3d7afb5114d244a3b38c5f9924424d9dcc9a
Jisu Lee <jisulee.shsf@gmail.com>
2022-12-29 00:07:59 +0900

    Upload file

---
+++ 02_leetcode/03_employees_earning_more_than_their_managers_221229.sql
@@ -0,0 +1,7 @@
+-- [03. Employees Earning More Than Their Managers]
+
+# submitted code
+SELECT e1.name AS Employee
+FROM Employee e1
+INNER JOIN Employee e2 ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary;
