f65731bb00a4a35121093916e7c8a975f077c49f
Jisu Lee <jisulee.shsf@gmail.com>
2022-12-28 21:09:43 +0900

    Upload file

---
+++ 01_hackerrank/01_top_earners_221228.sql
@@ -0,0 +1,18 @@
+-- [01. Top Earners]
+
+# version 1
+SELECT months * salary AS earnings
+     , COUNT(*)
+FROM employee
+GROUP BY earnings
+ORDER BY earnings DESC
+LIMIT 1;
+
+# version 2
+SELECT earnings
+     , COUNT(*)
+FROM (SELECT months * salary AS earnings
+           , RANK() OVER (ORDER BY months * salary DESC) AS ranking
+      FROM employee) tbl
+WHERE ranking = 1
+GROUP BY earnings;
