4bf22e62c35d93809324f4f7fd5f73a4815dfd10
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-12-28 21:13:28 +0900

    Update 01_top_earners_221228.sql

--- 01_hackerrank/01_top_earners_221228.sql
+++ 01_hackerrank/01_top_earners_221228.sql
@@ -3,7 +3,7 @@
 # version 1
 SELECT months * salary AS earnings
      , COUNT(*)
-FROM employee
+FROM Employee
 GROUP BY earnings
 ORDER BY earnings DESC
 LIMIT 1;
@@ -13,6 +13,6 @@ SELECT earnings
      , COUNT(*)
 FROM (SELECT months * salary AS earnings
            , RANK() OVER (ORDER BY months * salary DESC) AS ranking
-      FROM employee) tbl
+      FROM Employee) tbl
 WHERE ranking = 1
 GROUP BY earnings;
