877054d87a77b164984f55afbafd14beab7708bb
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-12-29 11:01:12 +0900

    Update 11*_Placements_221229.sql

--- 01_hackerrank/11*_Placements_221229.sql
+++ 01_hackerrank/11*_Placements_221229.sql
@@ -7,4 +7,4 @@ INNER JOIN Packages p1 ON s.ID = p1.ID
 INNER JOIN Friends f ON p1.ID = f.ID
 INNER JOIN Packages p2 ON f.Friend_ID = p2.ID
 WHERE p2.Salary > p1.Salary
-ORDER BY p2.Salary
+ORDER BY p2.Salary;
