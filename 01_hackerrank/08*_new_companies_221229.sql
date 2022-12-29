1176bea56adc82b3c9a15936d6e2eb99c712876d
Jisu Lee <jisulee.shsf@gmail.com>
2022-12-29 09:20:27 +0900

    Upload file

---
+++ 01_hackerrank/08*_new_companies_221229.sql
@@ -0,0 +1,11 @@
+-- [08. New Companies]
+
+# submitted code
+SELECT c.company_code
+     , c.founder
+     , (SELECT COUNT(DISTINCT lead_manager_code) FROM Lead_Manager WHERE company_code = c.company_code)
+     , (SELECT COUNT(DISTINCT senior_manager_code) FROM Senior_Manager WHERE company_code = c.company_code)
+     , (SELECT COUNT(DISTINCT manager_code) FROM Manager WHERE company_code = c.company_code)
+     , (SELECT COUNT(DISTINCT employee_code) FROM Employee WHERE company_code = c.company_code)
+FROM company c
+ORDER BY company_code;
