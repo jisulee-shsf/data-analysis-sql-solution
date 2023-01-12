67ae01bbf116c26013ef8d7e8080e3d92b57d9f8
Jisu Lee <jisulee.shsf@gmail.com>
2023-01-13 01:54:41 +0900

    Upload file

---
+++ 03_programmers/06_없어진_기록_찾기_230113.sql
@@ -0,0 +1,9 @@
+-- [06. 없어진 기록 찾기]
+
+# submitted code
+SELECT o.ANIMAL_ID
+     , o.NAME
+FROM ANIMAL_OUTS o
+LEFT JOIN ANIMAL_INS i ON o.ANIMAL_ID = i.ANIMAL_ID
+WHERE i.ANIMAL_ID IS NULL
+ORDER BY 1;
