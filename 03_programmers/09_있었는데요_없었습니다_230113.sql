50785d48c74e8941db301e3fe26918319413d26e
Jisu Lee <jisulee.shsf@gmail.com>
2023-01-13 04:19:21 +0900

    Upload file

---
+++ 03_programmers/09_있었는데요_없었습니다_230113.sql
@@ -0,0 +1,9 @@
+-- [09. 있었는데요 없었습니다]
+
+# submitted code
+SELECT i.ANIMAL_ID
+     , i.NAME
+FROM ANIMAL_INS i
+INNER JOIN ANIMAL_OUTS o ON i.ANIMAL_ID = o.ANIMAL_ID
+WHERE i.DATETIME > o.DATETIME
+ORDER BY i.DATETIME;
