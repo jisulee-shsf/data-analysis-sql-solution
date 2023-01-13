05d92513224dc33fc3acb9b790eec637eb74bafe
Jisu Lee <jisulee.shsf@gmail.com>
2023-01-13 22:14:31 +0900

    Upload file

---
+++ 03_programmers/14_루시와_엘라_찾기_230113.sql
@@ -0,0 +1,8 @@
+-- [14. 루시와 엘라 찾기]
+
+# submitted code
+SELECT ANIMAL_ID
+     , NAME
+     , SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty');
